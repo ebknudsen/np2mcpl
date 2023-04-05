866de23e0e2acb9468410b566893fd3708488c6e
erkn <erik@vonbrun.org>
2023-04-05 21:35:22 +0200

    make sure directions has length 1 in test

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import numpy2mcpl
 import numpy as np
-import math
 
+l1 = np.linalg.norm( (4,5,6,) )
+l2 = np.linalg.norm( (6,3,5,) )
 
-system_p = np.array([[1., 2., 3.,   4./math.sqrt(), 5./math.sqrt(15.), 6./math.sqrt(15.),    7., 9., 4., 0.,1.,0. ],
-                     [3., 2., 5.,   6./math.sqrt(14.), 3./math.sqrt(14.), 5./math.sqrt(14.),    6., 3., 5., 1.,0.,0.]])
+system_p = np.array([[1., 2., 3.,   4./l1, 5./l1, 6./l1,    7., 9., 4., 0.,1.,0. ],
+                     [3., 2., 5.,   6./l2, 3./l2, 5./l2,    6., 3., 5., 1.,0.,0.]])
 
 numpy2mcpl.numpy2mcpl_dump("thefile",system_p)
 
